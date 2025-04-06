75c21a0202abfdcd3959043879388054785fe722
bryku <drburnett@mail.com>
2025-04-05 20:20:31 -0700

    Update qRoute-1.js

--- qRoute-1.js
+++ qRoute-1.js
@@ -1,77 +1,80 @@
 // https://github.com/bryku/qRoute
+// https://github.com/bryku/qRoute
+// highjack clicks
+window.onclick = function(event){
+	let link = event.target.closest('a');
+	if(link){
+		let href = link.getAttribute('href');
+		if(href.startsWith('/')){
+			event.preventDefault();
+			window.history.pushState({}, '', window.location.origin + "/?!=" + href);
+			qRoute.load(window.location.origin + "/?!=" + href);
+		}
+	}
+}
+// highjack back button
+window.addEventListener('popstate', (event)=>{
+	qRoute.load(window.location.href);
+});
 let qRoute = {
-	onIndex: '',
-	onError: '',
-	getParams: {},
-	urlParams: {},
+	debug: false,
+	log: function(...arr){
+		if(this.debug){
+			console.warn('qRoute: Debug Mode\n'+arr.map(v => '    - '+v).join('\n'));
+		}
+	},
+	data: {
+		get: {},
+		url: {},
+	},
 	paths: [],
-	path: function(path, call){
+	path: function(path, call, fall = false){
 		this.paths.push({
 			path: path,
-			dirs: path.replace(/^\//gm, '').split('/'),
+			dirs: path.slice(1).split('/'),
 			call: call,
+			fall: fall
 		});
 	},
-	load: function(index = '/index', error = '/error'){
-		this.onIndex = index;
-		this.onError = error;
-		
-		let urlString = window.location.href;
+	load: function(url = window.location.href, data){
+		let urlString = url;
 		let urlObject = new URL(urlString);
 		let getObject = new URLSearchParams(urlObject.search);
-			getObject.forEach((value, key)=>{ this.getParams[key] = value; });
-			
-		this.redirect(this.getParams['!'] || this.onIndex);
-		this.highjack();
+			getObject.forEach((value, key)=>{
+				this.data.get[key] = value;
+			});
+		this.redirect(this.data.get['!'] || '/', data);
 	},
 	redirect: function(path, data){
-		let dirArray = path.replace(/^\//gm, '').split('/');
-		
-		let match = this.paths.find((path)=>{
-			return path.dirs.every((dir, dirIndex)=>{
-				if(!dirArray[dirIndex]){ return false }
-				if(dir == dirArray[dirIndex]){ return true }
-				if(dir[0] == ":"){
-					this.urlParams[dir.slice(1)] = dirArray[dirIndex];
+		let dirs = path.slice(1).split('/');
+		let page = this.paths.find((p)=>{
+			if(p.path == path){return true}
+			return p.dirs.every((dir, di)=>{
+				if(!dirs[di]){ return false }
+				if(dirs[di] == dir){ return true }
+				if(dir[0] == '?'){
+					this.data.url[dir.slice(1)] = dirs[di];
 					return true
 				}
-				return false;
+				return false
 			});
 		});
-		if(!match){
-			match = this.paths.find((path)=>{
-				return path.path == this.onError
-			});
+		if(!page){
+			this.log('note: Path Not Found','path: '+path);
+			page = this.paths.find(path => path.fall);
 		}
-		if(!match){
-			console.log('Router: Page Not Found - '+this.onError);
+		if(!page){
+			this.log('note: Fallback Not Found');
 		}else{
-			window.history.pushState({}, '', window.location.origin+'?!='+path);
-			match.call({
-				path: path,
-				url: this.urlParams, 
-				get: this.getParams,
+			this.log('note: Page Found', 'path: '+page.path);
+			page.call({
 				data: data,
+				path: path,
+				dirs: dirs,
+				url: this.data.url,
+				get: this.data.get,
 			});
-		}
-	},
-	highjack: function(){
-		document.querySelectorAll('a').forEach((link)=>{
-			let path = link.getAttribute('href');
-			if(path.startsWith('/')){
-				link.onclick = (event)=>{
-					event.preventDefault();
-					let a = event.target.closest('a');
-					this.redirect(a.getAttribute('href'))
-				};
-			}
-		});
-		// scrolls to element
-		if(window.location.hash){
-			let target = document.querySelector(window.location.hash);
-			if(target){
-				target.scrollIntoView()
-			}
+			return page
 		}
 	},
 };
